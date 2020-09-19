723f5f4ae858f1714d7d15a5c4ef26722bb78abd
chipo <pnapoli33@gmail.com>
2020-09-19 16:28:51 -0300

    video #306 - Implement Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,14 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClientesComponent } from './clientes/clientes.component';
-import {ClienteService} from './clientes/cliente.service'
+import { ClienteService } from './clientes/cliente.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'clientes', pathMatch: 'full' },
+  { path: 'directivas', component: DirectivaComponent },
+  { path: 'clientes', component: ClientesComponent },
+];
 
 @NgModule({
   declarations: [
@@ -14,12 +21,10 @@ import {ClienteService} from './clientes/cliente.service'
     HeaderComponent,
     FooterComponent,
     DirectivaComponent,
-    ClientesComponent
+    ClientesComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [ClienteService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
