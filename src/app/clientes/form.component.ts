ab1b20bb856447cfa7fa9e0ecf41fb8008a50742
chipo <pnapoli33@gmail.com>
2020-09-19 23:24:07 -0300

    video #325.1 - Work with form component and its html

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
 
 @Component({
   selector: 'app-form',
@@ -6,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  public cliente: Cliente = new Cliente()
+  public titulo:string = "Crear Cliente"
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public create(): void {
+    console.log("Clicked!")
+    console.log(this.cliente)
   }
 
 }
