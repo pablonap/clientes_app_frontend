0302d36be6ca2d2c8ebe6b9a6968ece7ec63b793
chipo <pnapoli33@gmail.com>
2020-09-19 21:12:11 -0300

    video #319.1 - Call API Rest from cliente.service.ts through HttpClient.

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,14 +2,18 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ClienteService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  private urlEndPoint:string = 'http://localhost:8080/api/clientes';
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    // return of(CLIENTES);
+    return this.http.get<Cliente[]>(this.urlEndPoint);
   }
 }
