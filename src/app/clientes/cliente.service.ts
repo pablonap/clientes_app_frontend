f1a9f6bfca7511300c95a1e78877972aee22dfc8
chipo <pnapoli33@gmail.com>
2020-09-20 12:07:25 -0300

    video #332 - Make Eliminar functionality

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -32,4 +32,8 @@ export class ClienteService {
   update(cliente: Cliente): Observable<Cliente>{
     return this.http.put<Cliente>(`${this.urlEndPoint}/${cliente.id}`, cliente, {headers: this.httpHeaders})
   }
+
+  delete(id: number): Observable<Cliente>{
+    return this.http.delete<Cliente>(`${this.urlEndPoint}/${id}`, {headers: this.httpHeaders})
+  }
 }
