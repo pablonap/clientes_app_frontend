53ef1b55a8cb3b2ed640b82b36289c3bf2b04d76
chipo <pnapoli33@gmail.com>
2020-09-20 01:24:21 -0300

    video #330 - Add functionality to Edit a Cliente so I worked in cliente.service in order to make the api call and I also work in clientes.component to upload the form with the Cliente retrieved from the backend.

--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
-import {Router} from '@angular/router'
+import {Router, ActivatedRoute} from '@angular/router'
 import swal from 'sweetalert2';
 
 @Component({
@@ -12,9 +12,22 @@ export class FormComponent implements OnInit {
   public cliente: Cliente = new Cliente();
   public titulo: string = 'Crear Cliente';
 
-  constructor(private clienteService: ClienteService, private router: Router) {}
+  constructor(private clienteService: ClienteService, 
+    private router: Router,
+    private activatedRoute: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.cargarCliente()
+  }
+
+  cargarCliente(): void{
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id']
+      if(id){
+        this.clienteService.getCliente(id).subscribe( (cliente) => this.cliente = cliente)
+      }
+    })
+  }
 
   public create(): void {
     this.clienteService.create(this.cliente).subscribe(
